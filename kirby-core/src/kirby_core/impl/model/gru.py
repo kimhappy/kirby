b4386a1748cc08033ef08f493e25c00ea8a69169
전현찬 <2020014975@hanyang.ac.kr>
2025-05-14 11:20:16 +0900

    first commit

---
+++ kirby-core/src/kirby_core/impl/model/gru.py
@@ -0,0 +1,33 @@
+import torch
+
+from ...protocol import _GeneralConfig, _ItemConfig, ModelBase
+
+class GRU(
+    ModelBase,
+    torch.nn.Module):
+    def __init__(
+        self                          ,
+        general_config: _GeneralConfig,
+        model_config  : _ItemConfig):
+        ModelBase      .__init__(self, general_config, model_config)
+        torch.nn.Module.__init__(self)
+
+        self.rec = torch.nn.GRU(
+            general_config.num_cond + 1,
+            model_config.hidden        ,
+            batch_first = True)
+        self.lin = torch.nn.Linear(
+            model_config.hidden,
+            1)
+        self.hidden = None
+
+    def forward(self, x):
+        rec_out, self.hidden = self.rec(x, self.hidden)
+        lin_out              = self.lin(rec_out)
+        return x[ ..., 0 ] + lin_out[ ..., 0 ]
+
+    def detach(self):
+        self.hidden = self.hidden.clone().detach()
+
+    def reset(self):
+        self.hidden = None
